a3d140cfadb90c7260d29475c8f89400abd362c5
mariavysotskaya <vysotskaya92@gmail.com>
2022-09-13 02:22:28 +0300

    cors fixed

--- backend/middlewares/cors-handle.js
+++ backend/middlewares/cors-handle.js
@@ -1,11 +1,10 @@
-/*
 const allowedCors = [
   'https://mymesto.nomoredomains.xyz',
   'http://mymesto.nomoredomains.xyz',
   'http://localhost:3000',
   'https://localhost:3000',
 ];
-*/
+
 /*
 module.exports = ((req, res, next) => {
   const { origin } = req.headers;
@@ -32,8 +31,10 @@ const cors = (req, res, next) => {
   const requestHeaders = req.headers['access-control-request-headers'];
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
-  res.header('Access-Control-Allow-Origin', origin);
-  res.header('Access-Control-Allow-Credentials', true);
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
 
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
